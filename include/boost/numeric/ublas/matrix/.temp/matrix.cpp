c8feeabb25924d4d1b112c3bec09ea794cc42d3f
relaxxpls <lolopaw24@gmail.com>
2021-03-16 02:08:44 +0530

    👌 IMPROVE: Add new features

--- include/boost/numeric/ublas/matrix/.temp/matrix.cpp
+++ include/boost/numeric/ublas/matrix/.temp/matrix.cpp
@@ -35,69 +35,73 @@ string ts(range r) {
     return "{"+ts(r.l)+","+ts(r.r)+','+ts(r.len)+"}";
 }
 
+#define _MN_ERR 1e-12
+
 template <typename T>
-class matrix {
+class Matrix {
+
 public:
-    matrix(size_t r, size_t c, T x) : R(r), C(c) {
+    // constructor with memory allocation
+    Matrix(size_t r, size_t c, T x=0) : R(r), C(c) {
         M.assign(R, std::vector<T>(C, x));
     }
 
+    // constructor from initializer_list
+    Matrix(const std::initializer_list<std::vector<T>> m) : M(m) {
+        R = M.size(), C = R > 0 ? M[0].size() : 0;
+    }
+
+    //  access size (rows, columns)
     bool empty() const {
         return R*C == 0;
     }
+
+    //  access size (rows, columns)
     std::pair<size_t, size_t> size() {
         return std::make_pair(R, C);
     }
-    matrix t() {
-        matrix res(C, R, 0);
-        for(size_t r=0; r<R; r++) {
-            for(size_t c=0; c<C; c++) {
-                res(c, r) += M[r][c];
-            }
-        }
-        return res;
-    }
 
-    matrix operator+=(const matrix &rhs) {
+    // algebric operators
+    Matrix operator+=(const Matrix &rhs) {
         assert(R == rhs.R && C == rhs.C);
-        for(size_t r=0; r<R; r++) {
-            for(size_t c=0; c<C; c++) {
+        for (size_t r = 0; r < R; r++) {
+            for (size_t c = 0; c < C; c++) {
                 M[r][c] += rhs(r, c);
             }
         }
         return *this;
     }
-    matrix operator-=(const matrix &rhs) {
+    Matrix operator-=(const Matrix &rhs) {
         assert(R == rhs.R && C == rhs.C);
-        for(size_t r=0; r<R; r++) {
-            for(size_t c=0; c<C; c++) {
+        for (size_t r=0; r<R; r++) {
+            for (size_t c=0; c<C; c++) {
                 M[r][c] -= rhs(r, c);
             }
         }
         return *this;
     }
-    matrix operator*=(const matrix &rhs) {
+    Matrix operator*=(const Matrix &rhs) {
         assert(C == rhs.R);
-        matrix res(R, rhs.C, 0);
-        for(size_t r=0; r<res.R; r++) {
-            for(size_t c=0; c<res.C; c++) {
-                for(size_t i=0; i<C; i++) {
+        Matrix res(R, rhs.C, 0);
+        for (size_t r=0; r<res.R; r++) {
+            for (size_t c=0; c<res.C; c++) {
+                for (size_t i=0; i<C; i++) {
                     res(r, c) += M[r][i] * rhs(i, c);
                 }
             }
         }
         return res;
     }
-    matrix operator+(const matrix &rhs) const {
-        return matrix(*this) += rhs;
+    Matrix operator+(const Matrix &rhs) const {
+        return Matrix(*this) += rhs;
     }
-    matrix operator-(const matrix &rhs) const {
-        return matrix(*this) -= rhs;
+    Matrix operator-(const Matrix &rhs) const {
+        return Matrix(*this) -= rhs;
     }
-    matrix operator*(const matrix &rhs) const {
-        return matrix(*this) *= rhs;
+    Matrix operator*(const Matrix &rhs) const {
+        return Matrix(*this) *= rhs;
     }
-    matrix operator*=(const T &rhs) {
+    Matrix operator*=(const T &rhs) {
         for(size_t r=0; r<R; r++) {
             for(size_t c=0; c<C; c++) {
                 M[r][c] *= rhs;
@@ -105,32 +109,30 @@ public:
         }
         return *this;
     }
-    matrix operator*(const T &rhs) const {
-        return matrix(*this) *= rhs;
+    Matrix operator*(const T &rhs) const {
+        return Matrix(*this) *= rhs;
     }
-    matrix operator-() const {
-        return matrix(R, C, 0) - *this;
+    Matrix operator-() const {
+        return Matrix(R, C, 0) - *this;
     }
 
-    bool operator==(const matrix &rhs) const {
-        if(R != rhs.R || C != rhs.C) return false;
-        for(size_t r=0; r<R; r++) {
-            for(size_t c=0; c<C; c++) {
-                if(M[r][c] != rhs(r, c)) {
-                    return false;
-                }
+    // L2 norm of A-B (ie ||A-B||^2) must be lesser than _MN_ERR (=1e-12)
+    bool operator==(const Matrix &rhs) const {
+        if (empty() || R != rhs.R || C != rhs.C) {
+            return false;
+        }
+        double L2 = 0;
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                L2 += (M[i, j] - rhs(i, j)) * (M[i, j] - rhs(i, j));
             }
         }
-        return true;
+        L2 /= R*C;
+        return L2 < _MN_ERR;
     }
-    bool operator!=(const matrix &rhs) const {
+    bool operator!=(const Matrix &rhs) const {
         return !(*this == rhs);
     }
-
-    matrix operator+=(const T &rhs);
-    matrix operator-=(const T &rhs);
-    matrix operator+(const T &rhs) const;
-    matrix operator-(const T &rhs) const;
 
     // Element Numbering (0 based, Column  Major):
     //    C 0  1  2   3
@@ -138,6 +140,8 @@ public:
     //  0   0  3  6   9
     //  1   1  4  7  10
     //  2   2  5  8  11
+
+    // access data operators
     T& operator[](size_t i) {
         return M[i % R][i / R];
     }
@@ -149,9 +153,9 @@ public:
         assert(0 <= r && r < R && 0 <= c && c < C);
         return M[r][c];
     }
-    matrix operator()(const range &row_range, const range &col_range) const {
+    Matrix operator()(const range &row_range, const range &col_range) const {
         assert(row_range.r < R && col_range.r < C);
-        matrix<T> res(row_range.len, col_range.len, 0);
+        Matrix<T> res(row_range.len, col_range.len, 0);
         for (size_t r = 0; r < row_range.len; r++) {
             for (size_t c = 0; c < col_range.len; c++)
                 res(r, c) = M[row_range.l + r][col_range.l + c];
@@ -159,25 +163,65 @@ public:
         return res;
     }
 
+    // compute transpose
+    Matrix t() {
+        Matrix res(C, R);
+        for(size_t r=0; r<R; r++) {
+            for(size_t c=0; c<C; c++) {
+                res(c, r) += M[r][c];
+            }
+        }
+        return res;
+    }
+
+    // compute dth minor
+    Matrix compute_minor(int d) {
+        Matrix res(R, C);
+        for (int i = 0; i < d; i++) {
+            res(i, i) = 1;
+        }
+        for (int r = d; r < R; r++) {
+            for (int c = d; c < C; c++) {
+                res(r, c) = M[r][c];
+            }
+        }
+        return res;
+    }
+
+    // return c-th column of m
+    std::vector<T> column(int c) const {
+        std::vector<T> v(R);
+        for (size_t r = 0; r < R; r++) {
+            v[r] = M[r][c];
+        }
+        return v;
+    }
+
+    // return r-th row of m
+    std::vector<T> row(int r) const {
+        return M[r];
+    }
+
+    // Matrix m to exponent n (Binary Exponentiation, O(logN))
     template <typename U>
-    friend matrix<U> pow(matrix<U> m, int n);
+    friend Matrix<U> pow(Matrix<U> m, int n);
 
     // To permit: 5*M, without this only M*5 is permitted
     template <typename U>
-    friend matrix<U> operator*(const U &lhs, const matrix<U> &rhs);
+    friend Matrix<U> operator*(const U &lhs, const Matrix<U> &rhs);
 
 private:
     size_t R, C;
     std::vector<std::vector<T>> M;
 };
 
-template <typename U>
-matrix<U> operator*(const U &lhs, const matrix<U> &rhs) {
+template <typename T>
+Matrix<T> operator*(const T &lhs, const Matrix<T> &rhs) {
     return rhs * lhs;
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream &cout, matrix<T> m) {
+std::ostream& operator<<(std::ostream &cout, Matrix<T> m) {
     size_t R = m.size().first, C = m.size().second;
     for(size_t r = 0; r < R; r++) {
         std::cout << "[";
@@ -194,16 +238,16 @@ std::ostream& operator<<(std::ostream &cout, matrix<T> m) {
     return cout;
 }
 
-// Zero/null matrix of size r*c
+// Zero/null Matrix of size r*c
 template <typename T>
-matrix<T> zeros(size_t r, size_t c) {
-    return matrix<T>(r, c, 0);
+Matrix<T> zeros(size_t r, size_t c) {
+    return Matrix<T>(r, c, 0);
 }
 
-// Identity matrix of size r*r
+// Identity Matrix of size r*r
 template<typename T>
-matrix<T> eye(size_t r) {
-    matrix<T> res(r, r, 0);
+Matrix<T> eye(size_t r) {
+    Matrix<T> res(r, r, 0);
     for(size_t i=0; i<r; i++) {
         res(i, i) = 1;
     }
@@ -211,12 +255,12 @@ matrix<T> eye(size_t r) {
 }
 
 template<typename T>
-matrix<T> pow(matrix<T> m, int n) {
+Matrix<T> pow(Matrix<T> m, int n) {
     assert(m.R == m.C && n >= 0);
     if(n == 0) {
         return eye<T>(m.R);
     }
-    matrix<T> res = m;
+    Matrix<T> res = m;
     for(int i=n; i>0; i>>=1) {
         res = (i&1) ? res*m : res;
         m *= m;
@@ -224,27 +268,24 @@ matrix<T> pow(matrix<T> m, int n) {
     return res;
 }
 
-// Norm of matrix m
+// Norm of Matrix m
 // Options available:
 // L1: Manhatten Norm
 // L2: Eucledian Norm (under development)
 // LF: Frobenius Norm
 // Linf: Maximum Norm
 template<typename T>
-double norm(matrix<T> m, string NORM = "LF") {
+double norm(Matrix<T> m, std::string NORM = "LF") {
     double res = 0;
     size_t R = m.size().first, C = m.size().second;
     if (NORM == "L1") {
         for(size_t c = 0; c < C; c++) {
             double cur = 0;
             for(size_t r = 0; r < R; r++) {
-                res += abs(m(r, c));
+                cur += abs(m(r, c));
             }
-            res = max(res, cur);
+            res = std::max(res, cur);
         }
-    }
-    else if (NORM == "L2") {
-
     }
     else if (NORM == "LF"){
         for(size_t r = 0; r < R; r++) {
@@ -258,9 +299,9 @@ double norm(matrix<T> m, string NORM = "LF") {
         for(size_t r = 0; r < R; r++) {
             double cur = 0;
             for(size_t c = 0; c < C; c++) {
-                res += abs(m(r, c));
+                cur += abs(m(r, c));
             }
-            res = max(res, cur);
+            res = std::max(res, cur);
         }
     }
     else {
@@ -269,17 +310,125 @@ double norm(matrix<T> m, string NORM = "LF") {
     return res;
 }
 
+// kth norm of vector v
+// k != inf: pow(sum(v^k), 1/k)
+// k == inf: max element in v
+template<typename T>
+double norm(std::vector<T> v, std::string NORM = "L2") {
+    double res = 0;
+    size_t n = v.size();
+    if(NORM == "L1") {
+        for(size_t i = 0; i < n; i++) {
+            res += std::abs(v[i]);
+        }
+    }
+    else if(NORM == "L2") {
+        for(size_t i = 0; i < n; i++) {
+            res += v[i] * v[i];
+        }
+        res = std::sqrt(res);
+    }
+    else if (NORM == "Linf") {
+        for(size_t i = 0; i < n; i++) {
+            res = std::max(res, v[i]);
+        }
+    }
+    else {
+        assert(false);
+    }
+    return res;
+}
+
+// MaxVal - MinVal over axis
+// axis 1: range of each column
+// axis 2: range of each row
+template<typename T>
+std::vector<T> range(Matrix<T> m, int axis = 1) {
+    assert(axis == 1 || axis == 2);
+    size_t R = m.size().first, C = m.size().second;
+    std::vector<T> res(axis == 1 ? C : R);
+
+    if(axis == 1) {
+        for(size_t c = 0; c < C; c++) {
+            T mnVal = std::numeric_limits<T>::max();
+            T mxVal = std::numeric_limits<T>::lowest();
+            for(size_t r = 0; r < R; r++) {
+                mnVal = std::min(mnVal, m(r, c));
+                mxVal = std::max(mxVal, m(r, c));
+            }
+            res[c] = mxVal - mnVal;
+        }
+    }
+    else {
+        for(size_t r = 0; r < R; r++) {
+            T mnVal = std::numeric_limits<T>::max();
+            T mxVal = std::numeric_limits<T>::lowest();
+            for(size_t c = 0; c < C; c++) {
+                mnVal = std::min(mnVal, m(r, c));
+                mxVal = std::max(mxVal, m(r, c));
+            }
+            res[r] = mxVal - mnVal;
+        }
+    }
+    return res;
+}
+
+// divide data by factor
+template<typename T>
+std::vector<T> rescale(const std::vector<T> &v, T factor=0) {
+    if(factor == 0) {
+        factor = norm(v);
+    }
+    assert(factor != 0);
+    std::vector<T> res(v);
+    for (int i = 0; i < v.size(); i++) {
+        res[i] /= factor;
+    }
+    return res;
+}
+
+// A(:, n) -> nth column of A
+//
+// template<typename T>
+// tuple<Matrix<T>, Matrix<T>> qr(Matrix<T> m) {
+//     size_t R = m.size().first, C = m.size().second;
+//     Matrix<T> mQ(R, C, 0), mR(C, C, 0);
+
+//     for(size_t k = 0; k < C; k++) {
+//         R(1:k-1, k) = Q(:, 1:k-1).t() * A(:,k);
+//         v = A(:,k) - Q(:,1:k-1) * R(1:k-1,k);
+//         R(k, k) = norm(v);
+//         Q(:,k) = v / R(k,k);
+
+
+
+//     }
+//     return std::make_tuple(mQ, mR);
+// }
+
 //============================//   Code End   //=============================//
 
 int main() {
-    matrix<int> B(3, 3, 2);
-    matrix<int> M = pow(B, 3);
+    Matrix<int> B(3, 3, 2);
+    Matrix<int> M = pow(B, 3);
     M(1, 0) = 1;
     M(2, 2) = 7;
     M(1, 2) = -2;
-    matrix<int> X = -2*M.t();
+    Matrix<int> X = -2*M.t();
     std::cout << X << std::endl;
 
-    matrix<int> y = X({1, 2}, {0, 2});
+    Matrix<int> y = X({1, 2}, {0, 2});
     std::cout << y << std::endl;
+
+    Matrix<int> D = {{65, 77, 69, 75, 69},
+                     {61, 74, 70, 66, 68},
+                     {81, 80, 71, 74, 79}};
+    std::cout << norm(D, "L1") << std::endl;
+    std::cout << range(D, 2)[0] << std::endl;
+    std::cout << D << std::endl;
+
+    // tuple<Matrix<int>, Matrix<int>> TQR= qr(D);
+    // Matrix<int> q = get<0>(TQR), r = get<1>(TQR);
+    // Matrix<int> q(3, 5), r(5, 5);
+    // std::tie(q, r) = qr(D);
 }
--- include/boost/numeric/ublas/matrix/matrix.hpp
+++ include/boost/numeric/ublas/matrix/matrix.hpp
@@ -8,12 +8,9 @@
 #include <cassert>
 #include <iterator>
 #include <cmath>
+#include <climits>
 
-/**
- *  Operations:
- *  1 Matrix(A), 1 Scalar(b): A+I*b, A-I*b, A*b
- *  2 Matrices(A, B): A+B, A-B, A*B
- */
+#define _MN_ERR 1e-12
 
 struct range {
     size_t l, r, len;
@@ -23,68 +20,72 @@ struct range {
 };
 
 template <typename T>
-class matrix {
+class Matrix {
+
 public:
-    matrix(size_t r, size_t c, T x) : R(r), C(c) {
+    // constructor with memory allocation
+    Matrix(size_t r, size_t c, T x=0) : R(r), C(c) {
         M.assign(R, std::vector<T>(C, x));
     }
 
+    // constructor from initializer_list
+    Matrix(const std::initializer_list<std::vector<T>> m) : M(m) {
+        R = M.size(), C = R > 0 ? M[0].size() : 0;
+    }
+
+    //  access size (rows, columns)
     bool empty() const {
         return R*C == 0;
     }
+
+    //  access size (rows, columns)
     std::pair<size_t, size_t> size() {
         return std::make_pair(R, C);
     }
-    matrix t() {
-        matrix res(C, R, 0);
-        for(size_t r=0; r<R; r++) {
-            for(size_t c=0; c<C; c++) {
-                res(c, r) += M[r][c];
-            }
-        }
-        return res;
-    }
 
-    matrix operator+=(const matrix &rhs) {
+    // Algebric Operations:
+    // 1 Matrix(A), 1 Scalar(b): A+I*b, A-I*b, A*b
+    // 2 Matrices(A, B): A+B, A-B, A*B
+    Matrix operator+=(const Matrix &rhs) {
         assert(R == rhs.R && C == rhs.C);
-        for(size_t r=0; r<R; r++) {
-            for(size_t c=0; c<C; c++) {
+        for (size_t r = 0; r < R; r++) {
+            for (size_t c = 0; c < C; c++) {
                 M[r][c] += rhs(r, c);
             }
         }
         return *this;
     }
-    matrix operator-=(const matrix &rhs) {
+    Matrix operator-=(const Matrix &rhs) {
         assert(R == rhs.R && C == rhs.C);
-        for(size_t r=0; r<R; r++) {
-            for(size_t c=0; c<C; c++) {
+        for (size_t r=0; r<R; r++) {
+            for (size_t c=0; c<C; c++) {
                 M[r][c] -= rhs(r, c);
             }
         }
         return *this;
     }
-    matrix operator*=(const matrix &rhs) {
+    Matrix operator*=(const Matrix &rhs) {
         assert(C == rhs.R);
-        matrix res(R, rhs.C, 0);
-        for(size_t r=0; r<res.R; r++) {
-            for(size_t c=0; c<res.C; c++) {
-                for(size_t i=0; i<C; i++) {
+        Matrix res(R, rhs.C, 0);
+        for (size_t r=0; r<res.R; r++) {
+            for (size_t c=0; c<res.C; c++) {
+                for (size_t i=0; i<C; i++) {
                     res(r, c) += M[r][i] * rhs(i, c);
                 }
             }
         }
         return res;
     }
-    matrix operator+(const matrix &rhs) const {
-        return matrix(*this) += rhs;
+    Matrix operator+(const Matrix &rhs) const {
+        return Matrix(*this) += rhs;
     }
-    matrix operator-(const matrix &rhs) const {
-        return matrix(*this) -= rhs;
+    Matrix operator-(const Matrix &rhs) const {
+        return Matrix(*this) -= rhs;
     }
-    matrix operator*(const matrix &rhs) const {
-        return matrix(*this) *= rhs;
+    Matrix operator*(const Matrix &rhs) const {
+        return Matrix(*this) *= rhs;
     }
-    matrix operator*=(const T &rhs) {
+    Matrix operator*=(const T &rhs) {
         for(size_t r=0; r<R; r++) {
             for(size_t c=0; c<C; c++) {
                 M[r][c] *= rhs;
@@ -92,32 +93,30 @@ public:
         }
         return *this;
     }
-    matrix operator*(const T &rhs) const {
-        return matrix(*this) *= rhs;
+    Matrix operator*(const T &rhs) const {
+        return Matrix(*this) *= rhs;
     }
-    matrix operator-() const {
-        return matrix(R, C, 0) - *this;
+    Matrix operator-() const {
+        return Matrix(R, C, 0) - *this;
     }
 
-    bool operator==(const matrix &rhs) const {
-        if(R != rhs.R || C != rhs.C) return false;
-        for(size_t r=0; r<R; r++) {
-            for(size_t c=0; c<C; c++) {
-                if(M[r][c] != rhs(r, c)) {
-                    return false;
-                }
+    // L2 norm of A-B (ie ||A-B||^2) must be lesser than _MN_ERR (=1e-12)
+    bool operator==(const Matrix &rhs) const {
+        if (empty() || R != rhs.R || C != rhs.C) {
+            return false;
+        }
+        double L2 = 0;
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                L2 += (M[i, j] - rhs(i, j)) * (M[i, j] - rhs(i, j));
             }
         }
-        return true;
+        L2 /= R*C;
+        return L2 < _MN_ERR;
     }
-    bool operator!=(const matrix &rhs) const {
+    bool operator!=(const Matrix &rhs) const {
         return !(*this == rhs);
     }
-
-    matrix operator+=(const T &rhs);
-    matrix operator-=(const T &rhs);
-    matrix operator+(const T &rhs) const;
-    matrix operator-(const T &rhs) const;
 
     // Element Numbering (0 based, Column  Major):
     //    C 0  1  2   3
@@ -125,6 +124,8 @@ public:
     //  0   0  3  6   9
     //  1   1  4  7  10
     //  2   2  5  8  11
+
+    // access data operators
     T& operator[](size_t i) {
         return M[i % R][i / R];
     }
@@ -136,9 +137,9 @@ public:
         assert(0 <= r && r < R && 0 <= c && c < C);
         return M[r][c];
     }
-    matrix operator()(const range &row_range, const range &col_range) const {
+    Matrix operator()(const range &row_range, const range &col_range) const {
         assert(row_range.r < R && col_range.r < C);
-        matrix<T> res(row_range.len, col_range.len, 0);
+        Matrix<T> res(row_range.len, col_range.len, 0);
         for (size_t r = 0; r < row_range.len; r++) {
             for (size_t c = 0; c < col_range.len; c++)
                 res(r, c) = M[row_range.l + r][col_range.l + c];
@@ -146,25 +147,65 @@ public:
         return res;
     }
 
+    // compute transpose
+    Matrix t() {
+        Matrix res(C, R);
+        for(size_t r=0; r<R; r++) {
+            for(size_t c=0; c<C; c++) {
+                res(c, r) += M[r][c];
+            }
+        }
+        return res;
+    }
+
+    // compute dth minor
+    Matrix compute_minor(int d) {
+        Matrix res(R, C);
+        for (int i = 0; i < d; i++) {
+            res(i, i) = 1;
+        }
+        for (int r = d; r < R; r++) {
+            for (int c = d; c < C; c++) {
+                res(r, c) = M[r][c];
+            }
+        }
+        return res;
+    }
+
+    // return c-th column of m
+    std::vector<T> column(int c) const {
+        std::vector<T> v(R);
+        for (size_t r = 0; r < R; r++) {
+            v[r] = M[r][c];
+        }
+        return v;
+    }
+
+    // return r-th row of m
+    std::vector<T> row(int r) const {
+        return M[r];
+    }
+
+    // Matrix m to exponent n (Binary Exponentiation, O(logN))
     template <typename U>
-    friend matrix<U> pow(matrix<U> m, int n);
+    friend Matrix<U> pow(Matrix<U> m, int n);
 
     // To permit: 5*M, without this only M*5 is permitted
     template <typename U>
-    friend matrix<U> operator*(const U &lhs, const matrix<U> &rhs);
+    friend Matrix<U> operator*(const U &lhs, const Matrix<U> &rhs);
 
 private:
     size_t R, C;
     std::vector<std::vector<T>> M;
 };
 
-template <typename U>
-matrix<U> operator*(const U &lhs, const matrix<U> &rhs) {
+template <typename T>
+Matrix<T> operator*(const T &lhs, const Matrix<T> &rhs) {
     return rhs * lhs;
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream &cout, matrix<T> m) {
+std::ostream& operator<<(std::ostream &cout, Matrix<T> m) {
     size_t R = m.size().first, C = m.size().second;
     for(size_t r = 0; r < R; r++) {
         std::cout << "[";
@@ -181,16 +222,16 @@ std::ostream& operator<<(std::ostream &cout, matrix<T> m) {
     return cout;
 }
 
-// Zero/null matrix of size r*c
+// Zero/null Matrix of size r*c
 template <typename T>
-matrix<T> zeros(size_t r, size_t c) {
-    return matrix<T>(r, c, 0);
+Matrix<T> zeros(size_t r, size_t c) {
+    return Matrix<T>(r, c, 0);
 }
 
-// Identity matrix of size r*r
+// Identity Matrix of size r*r
 template<typename T>
-matrix<T> eye(size_t r) {
-    matrix<T> res(r, r, 0);
+Matrix<T> eye(size_t r) {
+    Matrix<T> res(r, r, 0);
     for(size_t i=0; i<r; i++) {
         res(i, i) = 1;
     }
@@ -198,12 +239,12 @@ matrix<T> eye(size_t r) {
 }
 
 template<typename T>
-matrix<T> pow(matrix<T> m, int n) {
+Matrix<T> pow(Matrix<T> m, int n) {
     assert(m.R == m.C && n >= 0);
     if(n == 0) {
         return eye<T>(m.R);
     }
-    matrix<T> res = m;
+    Matrix<T> res = m;
     for(int i=n; i>0; i>>=1) {
         res = (i&1) ? res*m : res;
         m *= m;
@@ -211,21 +252,21 @@ matrix<T> pow(matrix<T> m, int n) {
     return res;
 }
 
-// Norm of matrix m
-// Options available currently: L1, LF, Linf
+// Norm of Matrix m
+// Options available:
 // L1: Manhatten Norm
 // L2: Eucledian Norm (under development)
 // LF: Frobenius Norm
 // Linf: Maximum Norm
 template<typename T>
-double norm(matrix<T> m, std::string NORM = "LF") {
+double norm(Matrix<T> m, std::string NORM = "LF") {
     double res = 0;
     size_t R = m.size().first, C = m.size().second;
     if (NORM == "L1") {
         for(size_t c = 0; c < C; c++) {
             double cur = 0;
             for(size_t r = 0; r < R; r++) {
-                res += abs(m(r, c));
+                cur += abs(m(r, c));
             }
             res = std::max(res, cur);
         }
@@ -236,19 +277,96 @@ double norm(matrix<T> m, std::string NORM = "LF") {
                 res += m(r, c) * m(r, c);
             }
         }
-        res = std::sqrt(res);
+        res = sqrt(res);
     }
     else if (NORM == "Linf") {
         for(size_t r = 0; r < R; r++) {
             double cur = 0;
             for(size_t c = 0; c < C; c++) {
-                res += abs(m(r, c));
+                cur += abs(m(r, c));
             }
             res = std::max(res, cur);
         }
     }
     else {
         assert(false);
+    }
+    return res;
+}
+
+// kth norm of vector v
+// k != inf: pow(sum(v^k), 1/k)
+// k == inf: max element in v
+template<typename T>
+double norm(std::vector<T> v, std::string NORM = "L2") {
+    double res = 0;
+    size_t n = v.size();
+    if(NORM == "L1") {
+        for(size_t i = 0; i < n; i++) {
+            res += std::abs(v[i]);
+        }
+    }
+    else if(NORM == "L2") {
+        for(size_t i = 0; i < n; i++) {
+            res += v[i] * v[i];
+        }
+        res = std::sqrt(res);
+    }
+    else if (NORM == "Linf") {
+        for(size_t i = 0; i < n; i++) {
+            res = std::max(res, v[i]);
+        }
+    }
+    else {
+        assert(false);
+    }
+    return res;
+}
+
+// MaxVal - MinVal over axis
+// axis 1: range of each column
+// axis 2: range of each row
+template<typename T>
+std::vector<T> range(Matrix<T> m, int axis = 1) {
+    assert(axis == 1 || axis == 2);
+    size_t R = m.size().first, C = m.size().second;
+    std::vector<T> res(axis == 1 ? C : R);
+
+    if(axis == 1) {
+        for(size_t c = 0; c < C; c++) {
+            T mnVal = std::numeric_limits<T>::max();
+            T mxVal = std::numeric_limits<T>::lowest();
+            for(size_t r = 0; r < R; r++) {
+                mnVal = std::min(mnVal, m(r, c));
+                mxVal = std::max(mxVal, m(r, c));
+            }
+            res[c] = mxVal - mnVal;
+        }
+    }
+    else {
+        for(size_t r = 0; r < R; r++) {
+            T mnVal = std::numeric_limits<T>::max();
+            T mxVal = std::numeric_limits<T>::lowest();
+            for(size_t c = 0; c < C; c++) {
+                mnVal = std::min(mnVal, m(r, c));
+                mxVal = std::max(mxVal, m(r, c));
+            }
+            res[r] = mxVal - mnVal;
+        }
+    }
+    return res;
+}
+
+// divide data by factor
+template<typename T>
+std::vector<T> rescale(const std::vector<T> &v, T factor=0) {
+    if(factor == 0) {
+        factor = norm(v);
+    }
+    assert(factor != 0);
+    std::vector<T> res(v);
+    for (int i = 0; i < v.size(); i++) {
+        res[i] /= factor;
     }
     return res;
 }
